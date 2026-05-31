cc01cc94f5a1303eefe89419dcd2888f5e073ad8
lucaslow1234-collab <lucaslow1234@hotmail.com>
2026-05-31 21:58:04 +0800

    Update app.py

--- app.py
+++ app.py
@@ -21,4 +21,8 @@ def home():
 
 
 if __name__ == "__main__":
-    app.run()
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
